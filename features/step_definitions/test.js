48c9b8dde75b576b78916158e9deb5b911fd7d5c
RubyCheng <rb_smile121@hotmail.com>
2017-04-17 16:34:27 +0800

    ..

--- features/step_definitions/test.js
+++ features/step_definitions/test.js
@@ -5,10 +5,11 @@ var {defineSupportCode} = require('cucumber');
 
 
 const deploey = require(process.cwd() + '/Contract/dynamicDeploey/deploey');
+Code.the_exchange_rate_is_alp_blp(1,2);
 
-//exchangRequest.deployContract();
+/*exchangRequest.deployContract();
 deploey.init(function(){
   console.log("Deploey！");
-});
+});*/
 //Code.the_exchange_rate_is_alp_blp(1,2);
 //Code.original_alp_account_of_A_is(1);
--- script/deployResult/address.js
+++ script/deployResult/address.js
@@ -1,3 +1,3 @@
-var Account_address = '0x302fa6a129dc933a2962c04ce616b2f713efd827';
-var Account0_address = '0x30e3dc81e8228a35af2688c0cd7683667c869e98';
-var exchange_address = '0x529c706863d5473acc674707ca98941d71426d4e';
+var Account_address = '0x35625e4534bd4649558313c6b7327f799a534bc8';
+var Account0_address = '0x6c6b000898ef50613d743632b1b322cf59e47be1';
+var exchange_address = '0x3f3dc6d4631c61e3a50af91ce666c3f6468df52b';
