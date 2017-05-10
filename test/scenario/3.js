1a015bcd8c610732ec00d64ae7b55671090cb01a
nidhogg5 <nidhogg5@yahoo.com.tw>
2017-05-10 21:02:01 +0800

    增加點數測試

--- test/scenario/3.js
+++ test/scenario/3.js
@@ -6,46 +6,78 @@ const fs = require('fs')
 const Web3 = require('web3')
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
 
-const exchange_abi = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', '..', 'build', 'exchange.abi')))
+const Account_abi = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', '..', 'build', 'Account.abi')))
+const Account = web3.eth.contract(Account_abi).at(JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'config.json'))).Account.address)
 
-const exchange = web3.eth.contract(exchange_abi).at(JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'config.json'))).exchange.address)
+const LoyaltyPoint_abi = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', '..', 'build', 'LoyaltyPoint.abi')))
+const LoyaltyPoint = web3.eth.contract(LoyaltyPoint_abi).at(Account.getLocalLoyaltyPoint())
 
-describe('Scenario 3 : Get Exchage Contract Function', function () {
+
+describe('Scenario 3 : Get LoyaltyPoint Contract Function', function () {
     this.timeout(0)
-    describe('Use to Function when Company A wants to exchange 10 points',function(){
-        it('should use successfully',function(done){
-            exchange.to("Company B",10,{
-                from:web3.eth.coinbase,
-                gas:1234567
-            },(err,result) => {
-                if(err !== undefined && err !== null)
+    describe('Use getName Function', function () {
+        it('should use successfully', function (done) {
+            LoyaltyPoint.getName((err, result) => {
+                if (err !== undefined && err !== null)
                     done(err)
-                if(result !== undefined && result !== null){
-                    //console.log(result)
+                if (result !== undefined && result !== null) {
+                    console.log(result)
                     done()
                 }
             })
         })
     })
-    describe('Use getAccount Function',function(){
-        it('should use successfully',function(done){
-            exchange.getAccount((err,result) => {
-                if(err !== undefined && err !== null)
+    describe('Use getPoints Function', function () {
+        it('should use successfully', function (done) {
+            console.log(LoyaltyPoint.getPoints())
+            LoyaltyPoint.getPoints((err, result) => {
+                if (err !== undefined && err !== null)
                     done(err)
-                if(result !== undefined && result !== null){
-                    //console.log(result)
+                if (result !== undefined && result !== null) {
+                    console.log(result)
                     done()
                 }
             })
         })
     })
-    describe('Use getPartnerAccount Function',function(){
-        it('should use successfully',function(done){
-            exchange.getPartnerAccount((err,result) => {
-                if(err !== undefined && err !== null)
+    describe('Use addPoints Function', function () {
+        it('should use successfully', function (done) {
+            LoyaltyPoint.addPoints(10, {
+                from: web3.eth.coinbase,
+                gas: 1234567
+            },(err, result) => {
+                if (err !== undefined && err !== null)
                     done(err)
-                if(result !== undefined && result !== null){
-                    //console.log(result)
+                if (result !== undefined && result !== null) {
+                    console.log(result)
+                    done()
+                }
+            })
+        })
+    })
+    describe('Use setPoints Function', function () {
+        it('should use successfully', function (done) {
+            LoyaltyPoint.setPoints(500, {
+                from: web3.eth.coinbase,
+                gas: 1234567
+            },(err, result) => {
+                if (err !== undefined && err !== null)
+                    done(err)
+                if (result !== undefined && result !== null) {
+                    console.log(result)
+                    done()
+                }
+            })
+        })
+    })
+    describe('Use getRate Function', function () {
+        it('should use successfully', function (done) {
+            console.log(LoyaltyPoint.getRate())
+            LoyaltyPoint.getRate((err, result) => {
+                if (err !== undefined && err !== null)
+                    done(err)
+                if (result !== undefined && result !== null) {
+                    console.log(result)
                     done()
                 }
             })
---
+++ test/scenario/4.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const path = require('path')
+const fs = require('fs')
+
+const Web3 = require('web3')
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+
+const exchange_abi = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', '..', 'build', 'exchange.abi')))
+const exchange = web3.eth.contract(exchange_abi).at(JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'config.json'))).exchange.address)
+
+describe('Scenario 4 : Get Exchage Contract Function', function () {
+    this.timeout(0)
+    describe('Use to Function when Company A wants to exchange 10 points', function () {
+        it('should use successfully', function (done) {
+            exchange.to("Company B", 10, {
+                from: web3.eth.coinbase,
+                gas: 1234567
+            }, (err, result) => {
+                if (err !== undefined && err !== null)
+                    done(err)
+                if (result !== undefined && result !== null) {
+                    console.log(result)
+                    done()
+                }
+            })
+        })
+    })
+    describe('Use getAccount Function', function () {
+        it('should use successfully', function (done) {
+            exchange.getAccount((err, result) => {
+                if (err !== undefined && err !== null)
+                    done(err)
+                if (result !== undefined && result !== null) {
+                    console.log(result)
+                    done()
+                }
+            })
+        })
+    })
+    describe('Use getPartnerAccount Function', function () {
+        it('should use successfully', function (done) {
+            exchange.getPartnerAccount((err, result) => {
+                if (err !== undefined && err !== null)
+                    done(err)
+                if (result !== undefined && result !== null) {
+                    console.log(result)
+                    done()
+                }
+            })
+        })
+    })
+})
