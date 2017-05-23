0eda2e307b0a4a5595a7b43cbc91fa03e5827121
autumn <dennis456852@gmail.com>
2017-05-23 14:50:37 +0800

    edit app.js

--- Testweb/app.js
+++ Testweb/app.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -8,7 +12,10 @@ var app = express();
 
 // view engine setup
 
-
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
