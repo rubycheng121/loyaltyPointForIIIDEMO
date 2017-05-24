2908076a876cefd4d084d702dccb3a2208cd61d8
autumn <dennis456852@gmail.com>
2017-05-24 09:41:04 +0800

    add mocha test in routes

--- Testweb/routes/index.js
+++ Testweb/routes/index.js
@@ -1,10 +1,19 @@
 var path = require('path');
 var express = require('express');
+var fs = require('fs');
+var Mocha = require('mocha');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.resolve('public', 'index.html'));
 });
-
+router.post('/mocha',function(req, res, next){
+  fs.writeFileSync('test.js',req.body.mocha);
+  var mocha = new Mocha({ui : 'bdd',reporter :　'list'});
+  mocha.addFile('test.js');
+  mocha.reporter().run();
+  console.log(mocha.reporter()._reporter)
+  //console.log(req.body.mocha);
+})
 module.exports = router;
