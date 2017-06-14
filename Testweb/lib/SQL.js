aeb4a63d589121708b957c966b94b15bfac1505e
nidhogg5 <nidhogg5@yahoo.com.tw>
2017-06-15 00:12:11 +0800

    前端整理

---
+++ Testweb/lib/SQL.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+var credentials = require("./credentials.js")
+
+var connection = mysql.createConnection({
+    host: credentials.SQL.host,
+    user: credentials.SQL.user,
+    password: credentials.SQL.password,
+    database: credentials.SQL.database
+});
+
+function dowmload() {
+    console.log("資料庫下載");
+}
+
+function upload () {
+    console.log("資料庫上傳");
+}
+
+module.exports = {
+    connection: connection,
+    download: download,
+    upload: upload
+}
