62f7db51a94267238fadd111dea05127207a9085
autumn <dennis456852@gmail.com>
2017-06-15 21:38:38 +0800

    delete cucumber.js and mocha.js

--- Testweb/public/javascripts/index.js
+++ Testweb/public/javascripts/index.js
@@ -5,43 +5,6 @@ var mochaEditor
 var $output
 var $mochaOutput
 
-function runFeature() {
-	$output.empty();
-	$('a[href="#output-tab"]').tab('show');
-
-	var featureSource = featureEditor.getValue();
-	var feature = Cucumber.FeatureParser.parse({
-		scenarioFilter: new Cucumber.ScenarioFilter({}),
-		source: featureSource,
-		uri: '/feature'
-	});
-
-	Cucumber.clearSupportCodeFns();
-	new Function(stepDefinitionsEditor.getValue())();
-	var supportCodeLibrary = Cucumber.SupportCodeLibraryBuilder.build({
-		cwd: '/',
-		fns: Cucumber.getSupportCodeFns()
-	});
-
-	var formatterOptions = {
-		colorsEnabled: true,
-		cwd: '/',
-		log: function (data) {
-			appendToOutput(ansiHTML(data));
-		},
-		supportCodeLibrary: supportCodeLibrary
-	};
-	var prettyFormatter = Cucumber.FormatterBuilder.build('pretty', formatterOptions);
-
-	var runtime = new Cucumber.Runtime({
-		features: [feature],
-		listeners: [prettyFormatter],
-		supportCodeLibrary: supportCodeLibrary
-	});
-	return runtime.start();
-};
-
-
 function appendToOutput(data) {
 	$output.append(data);
 	$output.scrollTop($output.prop("scrollHeight"));
@@ -77,20 +40,25 @@ $(function () {
 	window.onerror = displayError;
 
 	$('#run-feature').click(function () {
-		runFeature().then(function (success) {
-			var exitStatus = success ? '0' : '1';
-			var exitStatusContainer = $('<div>');
-			exitStatusContainer.addClass('exit-status').text('Exit Status: ' + exitStatus);
-			appendToOutput(exitStatusContainer);
-		}).catch(displayError);
+		$.post('/cucumber', {
+			featureSource: featureEditor.getValue(),
+			stepDefinitions: stepDefinitionsEditor.getValue()
+		}, (result) => {
+			console.log(result)
+		})
 	});
-
 	$('#run-mocha').click(function () {
 		$.post("/mocha", {
-			mocha: mochaEditor.getValue(),
+			mocha: mochaEditor.getValue()
+		}, (result) => {
+			console.log(result)
+		});
+	});
+	$('#compile').click(function () {
+		$.post("/compile", {
 			solidity: solidityEditor.getValue()
 		}, (result) => {
-			appendToMochaOutput(result)
+			console.log(result)
 		});
 	});
 
