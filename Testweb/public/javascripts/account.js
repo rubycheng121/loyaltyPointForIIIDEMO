0baa3f3a4ee8155d56dc92552305179cb7b388c1
nidhogg5 <nidhogg5@yahoo.com.tw>
2017-06-22 22:59:55 +0800

    sign

--- Testweb/public/javascripts/account.js
+++ Testweb/public/javascripts/account.js
@@ -5,10 +5,7 @@ $(function () {
     var sign_up_form;
 
     var emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    var name = $("#name");
-    var email = $("#email");
-    var password = $("#password");
-    var allFields = $([]).add(name).add(email).add(password);
+    var allFields = $([]).add($("#sign_up_name")).add($("#sign_up_email")).add($("#sign_up_password"));
     var tips = $(".validateTips");
 
     //dialog
@@ -67,10 +64,18 @@ $(function () {
 
     //submit
     function sign_in_submit() {
+        allFields.removeClass("ui-state-error");
 
+        console.log($("#sign_in_name").val());
+        console.log($("#sign_in_password").val());
     }
-    function sign_up_submit() {
 
+    function sign_up_submit() {
+        allFields.removeClass("ui-state-error");
+
+        console.log($("#sign_up_name").val());
+        console.log($("#sign_up_email").val());
+        console.log($("#sign_up_password").val());
     }
 
     function addUser() {
@@ -124,31 +129,5 @@ $(function () {
             return true;
         }
     }
-
-    function addUser() {
-        var valid = true;
-        allFields.removeClass("ui-state-error");
-
-        valid = valid && checkLength(name, "username", 3, 16);
-        valid = valid && checkLength(email, "email", 6, 80);
-        valid = valid && checkLength(password, "password", 5, 16);
-
-        valid = valid && checkRegexp(name, /^[a-z]([0-9a-z_\s])+$/i,
-            "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter.");
-        valid = valid && checkRegexp(email, emailRegex, "eg. ui@jquery.com");
-        valid = valid && checkRegexp(password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9");
-
-        if (valid) {
-            $("#users tbody").append("<tr>" +
-                "<td>" + name.val() + "</td>" +
-                "<td>" + email.val() + "</td>" +
-                "<td>" + password.val() + "</td>" +
-                "</tr>");
-            dialog.dialog("close");
-        }
-        return valid;
-    }
-
-
 
 });
