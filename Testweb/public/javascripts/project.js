ffe434f47fa58c5708c17db4185715ebe3e1f548
nidhogg5 <nidhogg5@yahoo.com.tw>
2017-07-01 13:23:34 +0800

    panel

--- Testweb/public/javascripts/project.js
+++ Testweb/public/javascripts/project.js
@@ -25,16 +25,18 @@ function update() {
     $.post("get_project_list", (data) => {
         $('#project_list').html("");
         for (let i in data) {
-            $('#project_list').append('<div id="project_list">' +
-                '<div class="list-group">' +
-                '<a href="/editor?project=' + data[i].project + '" class="list-group-item">' +
-                '<h4 class="list-group-item-heading">' + data[i].project + '</h4>' +
-                '<p class="list-group-item-text">Create date : ' +  new Date(data[i].create_date).toLocaleString() + '</p>' +
-                '<p class="list-group-item-text">Last update : ' +  new Date(data[i].last_update).toLocaleString() + '</p>' +
-                '</a>' +
-                '</div>' +
+            $('#project_list').append(
+                '<div class="panel panel-info">' +
+                    '<div class="panel-heading">' +
+                        '<a href="/editor?project=' + data[i].project + '">' +
+                            '<h4 class="list-group-item-heading">' + data[i].project + '</h4>' +
+                        '</a>' +
+                    '</div>' +
+                    '<div class="panel-body">' +
+                        '<p class="list-group-item-text">Create date : ' + new Date(data[i].create_date).toLocaleString() + '</p>' +
+                        '<p class="list-group-item-text">Last update : ' + new Date(data[i].last_update).toLocaleString() + '</p>' +
+                    '</div>' +
                 '</div>');
-
         }
     });
 
