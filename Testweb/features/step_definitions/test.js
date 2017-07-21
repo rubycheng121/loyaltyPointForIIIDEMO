a63fa03af814ec87fdd729f88af41362e03bdfa6
nidhogg5 <nidhogg5@yahoo.com.tw>
2017-07-21 10:26:17 +0800

    ignore

---
+++ Testweb/features/step_definitions/test.js
@@ -0,0 +1,203 @@
+const { defineSupportCode } = require('cucumber');
+const assert = require('assert');
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+let Exchange_abi
+let Exchange_bytecode
+let Exchange_contract
+let Exchange_address
+
+let Account_abi
+let Account_bytecode
+let Account_contract
+let Account_address
+
+let LoyaltyPoint_abi
+let LoyaltyPoint_bytecode
+let LoyaltyPoint_contract
+let LoyaltyPoint_address
+
+defineSupportCode(function ({ Given, When, Then, And }) {
+
+    Given('the exchange rate is {int}alp={float}blp', function (int, float, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      callback(null, 'pending');
+    });
+
+
+    Given('original alp account of A is {int}', function (int, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      callback(null, 'pending');
+    });
+
+
+    Given('original blp account of A is {int}', function (int, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      callback(null, 'pending');
+    });
+
+
+    Given('original alp account of B is {int}', function (int, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      callback(null, 'pending');
+    });
+
+
+    Given('original blp account of B is {int}', function (int, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      callback(null, 'pending');
+    });
+
+
+    When('A want to exchange {int} alp for blp', function (int, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      callback(null, 'pending');
+    });
+
+
+    Then('alp account of A should be {int}', function (int, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      callback(null, 'pending');
+    });
+
+
+    Then('blp account of A should be {int}', function (int, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      callback(null, 'pending');
+    });
+
+
+    Then('alp account of B should be {int}', function (int, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      callback(null, 'pending');
+    });
+
+
+     Then('blp account of B should be {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Given('the exchange rate is {int}alp={float}blp', function (int, float, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Given('original alp account of A is {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Given('original blp account of A is {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Given('original alp account of B is {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Given('original blp account of B is {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     When('A want to exchange {int} alp for blp', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Then('alp account of A should be {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Then('blp account of A should be {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Then('alp account of B should be {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Then('blp account of B should be {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Given('the exchange rate is {int}alp={float}blp', function (int, float, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Given('original alp account of A is {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Given('original blp account of A is {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Given('original alp account of B is {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Given('original blp account of B is {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     When('A want to exchange {int} alp for blp', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Then('alp account of A should be {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Then('blp account of A should be {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Then('alp account of B should be {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+     Then('blp account of B should be {int}', function (int, callback) {
+       // Write code here that turns the phrase above into concrete actions
+       callback(null, 'pending');
+     });
+
+
+});
